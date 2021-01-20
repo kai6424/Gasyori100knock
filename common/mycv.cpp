0b7d4519aca771606f62da5455537c4ef4ed29fa
kai6424 <kai6424@gmail.com>
2021-01-21 00:12:36 +0900

    fix answer 9

--- common/mycv.cpp
+++ common/mycv.cpp
@@ -412,4 +412,70 @@ namespace mycv
         }
         return out;
     }
+
+    // gaussian filter
+    cv::Mat gaussian_filter(cv::Mat img, double sigma, int kernel_size)
+    {
+        int height = img.rows;
+        int width = img.cols;
+        int channel = img.channels();
+
+        // prepare output
+        cv::Mat out = cv::Mat::zeros(height, width, CV_8UC3);
+
+        // prepare kernel
+        int pad = floor(kernel_size / 2);
+        int _x = 0, _y = 0;
+        double kernel_sum = 0;
+
+        // get gaussian kernel
+        float kernel[kernel_size][kernel_size];
+
+        for (int y = 0; y < kernel_size; y++)
+        {
+            for (int x = 0; x < kernel_size; x++)
+            {
+                _y = y - pad;
+                _x = x - pad;
+                kernel[y][x] = 1 / (2 * M_PI * sigma * sigma) * exp(-(_x * _x + _y * _y) / (2 * sigma * sigma));
+                kernel_sum += kernel[y][x];
+            }
+        }
+
+        for (int y = 0; y < kernel_size; y++)
+        {
+            for (int x = 0; x < kernel_size; x++)
+            {
+                kernel[y][x] /= kernel_sum;
+            }
+        }
+
+        // filtering
+        double v = 0;
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int c = 0; c < channel; c++)
+                {
+
+                    v = 0;
+
+                    for (int dy = -pad; dy < pad + 1; dy++)
+                    {
+                        for (int dx = -pad; dx < pad + 1; dx++)
+                        {
+                            if (((x + dx) >= 0) && ((y + dy) >= 0))
+                            {
+                                v += (double)img.at<cv::Vec3b>(y + dy, x + dx)[c] * kernel[dy + pad][dx + pad];
+                            }
+                        }
+                    }
+                    out.at<cv::Vec3b>(y, x)[c] = v;
+                }
+            }
+        }
+        return out;
+    }
 } // namespace mycv
